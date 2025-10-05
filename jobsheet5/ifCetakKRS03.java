7223f09a6cf3ec768827ee9fdf8a4085ec8b0408
Vallent12 <fevadtya09.gmail.com>
2025-10-05 18:30:37 +0700

    Modifikasi percobaan 1

--- jobsheet5/ifCetakKRS03.java
+++ jobsheet5/ifCetakKRS03.java
@@ -10,9 +10,12 @@ public class ifCetakKRS03 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas); {
+        if (uktLunas) {
             System.out.println("Pembayaran UKT Terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
 
     }
